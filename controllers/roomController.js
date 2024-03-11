3d571f4f712439a4fea7883ada84b9714fda7c5b
Yeison Rojas <yeisonjr98@gmail.com>
2024-03-11 18:55:57 -0400

    add delete room

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,5 +1,5 @@
 const Room = require('../models/Room');
-//const fs = require('fs');
+const fs = require('fs');
 
 async function addRoom (req, res) {
   
@@ -57,8 +57,39 @@ async function getRooms(req, res) {
 
 }
 
+async function deleteRoom (req, res) {
+
+  const { id } = req.params;
+
+  try {
+
+    // elimina de la BDD
+    const deletedRoom = await Room.findOneAndDelete({ _id: id });
+
+    if(!deleteRoom) return res.status(400).json({ message:'Room not found' });
+    
+    // eliminar la imagen del server
+    // subcadena = filename
+    const subcadena = deletedRoom.image.split('/');
+
+    // path image
+    console.log(`./uploads/${subcadena[4]}`);
+
+    fs.unlink(`./uploads/${subcadena[4]}`, (err)=>{
+      if (err) return console.log('Image not found');
+      console.log(`${subcadena[4]} deleted`);
+    });
+
+    return res.status(200).json({ deletedRoom });
+    
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+}
+
 module.exports = {
   addRoom,
   getRoom,
-  getRooms
+  getRooms,
+  deleteRoom
 }
--- routes/room.js
+++ routes/room.js
@@ -3,10 +3,11 @@ const roomRouter = express.Router();
 const upload = require('../libs/storage');
 const verifyToken = require('../middleware/verifyToken');
 
-const { addRoom, getRoom, getRooms } = require('../controllers/roomController');
+const { addRoom, getRoom, getRooms, deleteRoom } = require('../controllers/roomController');
 
 roomRouter.get('/', verifyToken, getRooms);
 roomRouter.get('/:id', verifyToken, getRoom);
 roomRouter.post('/add', verifyToken, upload.single('image'), addRoom);
+roomRouter.post('/delete/:id', verifyToken, deleteRoom);
 
 module.exports = roomRouter;
